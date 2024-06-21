95efc8f4eca2f817bda38ff4d0d0190f336e1944
aymensellaouti <aymen.sellaouti@gmail.com>
2024-06-21 10:00:35 +0200

    correction exo login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+login(form: NgForm) {
+  console.log(form.value);
+
+}
   constructor() {}
 }
